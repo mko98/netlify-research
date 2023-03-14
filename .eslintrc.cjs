ef3fcc20727efcd49456c0de13b80680c6763700
Max Koning <maxkoning@casema.nl>
2023-03-14 11:19:40 +0100

    Project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto space-y-2"
+  >
+    <h1 class="text-3xl text-center">New Nuxt3 Project</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
